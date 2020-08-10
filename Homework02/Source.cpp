b6cd710dfac24ac9dfd50825523e59b8a6117b12
Thanpoochom <Thanpoochom@DESKTOP-3LGVV93>
2020-08-10 14:58:59 +0700

    Add project files.

---
+++ Homework02/Source.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ main()
+ {
+	 char test[] = "Chom";
+	 printf("String : %s\n", test);
+	 printf("test[0] = %c\n", test[0]);
+	 printf("test[1] = %c\n", test[1]);
+	 printf("test[2] = %c\n", test[2]);
+	 printf("test[3] = %c", test[3]);
+}
